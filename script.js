e3be75a4bd59cb4d17e89333a836c3fb3be6cbdd
Chris Fanucchi <admin@knightskeep.com>
2022-08-03 16:35:12 -0700

    Refined website response to anonymous users

--- script.js
+++ script.js
@@ -16,16 +16,19 @@ document.querySelector('.login').addEventListener('click', function () {
       document.querySelector(
         '#header-text'
       ).textContent = `Photography For ${loginName}`;
-      document.querySelector(
-        '#welcome-text'
-      ).textContent = `Welcome ${loginName}!`;
-      const welcomeText = document.querySelector('#welcome-text');
+      welcomeText.textContent = `Welcome ${loginName}!`;
       welcomeText.classList.remove('hidden');
     } else {
       document.querySelector(
         '#header-text'
       ).textContent = `Photography For All`;
-      welcomeText.classList.add('hidden');
+      welcomeText.textContent = `Welcome ${
+        loginName || 'anonymous'
+      }! You are not yet an authorized user on this site. Please submit your email address below.`;
+      welcomeText.classList.remove('hidden');
     }
+  } else {
+    welcomeText.classList.add('hidden');
+    document.querySelector('#header-text').textContent = `Photography For All`;
   }
 });

e77b6444f5e92b62906b1fedc75c14cfe4a50fd0
Chris Fanucchi <admin@knightskeep.com>
2022-08-04 15:32:10 -0700

    Added rating functionality with loops

--- script.js
+++ script.js
@@ -3,14 +3,11 @@
 const okUsers = ['chris', 'kassie', 'ben'];
 const welcomeText = document.querySelector('#welcome-text');
 const headerText = document.querySelector('#header-text');
-welcomeText.classList.add('hidden');
 let text;
+let rating;
+let stars = '';
+welcomeText.classList.add('hidden');
 
-document.querySelector('.check').addEventListener('click', function () {
-  alert(
-    `Sending tons of spam email to ${document.querySelector('.email').value}!!`
-  );
-});
 document.querySelector('.login').addEventListener('click', function () {
   let loginName = prompt('Please enter your first name (hint: me or staff):');
   if (loginName != null) {
@@ -20,6 +17,29 @@ document.querySelector('.login').addEventListener('click', function () {
       changeText(loginName || 'Anonymous', false);
     }
   }
+});
+
+document.querySelector('.rate').addEventListener('click', function () {
+  // const rating = Number(document.querySelector('.topicRate').value);
+  while (!(rating <= 5 && rating >= 1)) {
+    rating = Number(
+      prompt('Please rate the Drone topic on a scale of 1 to 5:')
+    );
+  }
+  for (let i = 1; i <= rating; i++) {
+    console.log(i);
+    stars += '⭐';
+  }
+  document.querySelector('#drone-rating').textContent = stars;
+  console.log(stars);
+  rating = 0;
+  stars = '';
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  alert(
+    `Sending tons of spam email to ${document.querySelector('.email').value}!!`
+  );
 });
 
 const changeText = function (name, onUserList) {
